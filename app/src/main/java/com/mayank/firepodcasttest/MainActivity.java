0aaf43f2cf399e87e19b4a1746df44b80a1aa1e2
mayankkanela <mayankkanela8@gmail.com>
2019-12-20 00:37:28 +0530

    final commit

--- app/src/main/java/com/mayank/firepodcasttest/MainActivity.java
+++ app/src/main/java/com/mayank/firepodcasttest/MainActivity.java
@@ -8,9 +8,12 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.Task;
@@ -26,9 +29,10 @@ public class MainActivity extends AppCompatActivity {
     StorageReference pathRef;
     TextView tv1;
     TextView tv2;
+    Handler handler;
     TextView tv3;
     MediaPlayer mediaPlayer;
-    ProgressBar progressBar;
+    SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 String url="https://firebasestorage.googleapis.com/v0/b/firepodcasttest.appspot.com/o/Audio%2FBECHARA%20RIBBON.mp3?alt=media&token=2ab40d7a-beb0-48e5-b5da-92a970bc43d2";
                 if(mediaPlayer!=null)
                     {
+                        mediaPlayer.stop();
                         mediaPlayer.release();
                         mediaPlayer=null;
                     }
@@ -66,6 +71,18 @@ public class MainActivity extends AppCompatActivity {
                     public void onPrepared(MediaPlayer mp) {
                         progressDialog.dismiss();
                         mediaPlayer.start();
+                        seekBar.setMax(mediaPlayer.getDuration());
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(mediaPlayer!=null)
+                                {
+                                    int pos=mediaPlayer.getCurrentPosition();
+                                    seekBar.setProgress(pos);
+                                }
+                                handler.postDelayed(this,200);
+                            }
+                        });
 
                     }
                 });
@@ -76,8 +93,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String url="https://firebasestorage.googleapis.com/v0/b/firepodcasttest.appspot.com/o/Audio%2FHAATHI%20AUR%20CHOOHE.mp3?alt=media&token=43325f6f-0052-4cce-b0c8-bca4e36536e3";
+
                 if(mediaPlayer!=null)
                 {
+                    mediaPlayer.stop();
                     mediaPlayer.release();
                     mediaPlayer=null;
                 }
@@ -97,7 +116,18 @@ public class MainActivity extends AppCompatActivity {
                     public void onPrepared(MediaPlayer mp) {
                         progressDialog.dismiss();
                         mediaPlayer.start();
-
+                        seekBar.setMax(mediaPlayer.getDuration());
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(mediaPlayer!=null)
+                                {
+                                    int pos=mediaPlayer.getCurrentPosition();
+                                    seekBar.setProgress(pos);
+                                }
+                                handler.postDelayed(this,200);
+                            }
+                        });
 
                     }
                 });
@@ -110,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
                 String url="https://firebasestorage.googleapis.com/v0/b/firepodcasttest.appspot.com/o/Audio%2FHow%20to%20tell%20a%20true%20princess%20edited%20version.mp3?alt=media&token=7c173ab7-5e3c-4cea-8614-b007e6b1dec2";
                 if(mediaPlayer!=null)
                 {
+                    mediaPlayer.stop();
                     mediaPlayer.release();
                     mediaPlayer=null;
                 }
@@ -129,6 +160,18 @@ public class MainActivity extends AppCompatActivity {
                     public void onPrepared(MediaPlayer mp) {
                         progressDialog.dismiss();
                         mediaPlayer.start();
+                        seekBar.setMax(mediaPlayer.getDuration());
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(mediaPlayer!=null)
+                                {
+                                    int pos=mediaPlayer.getCurrentPosition();
+                                    seekBar.setProgress(pos);
+                                }
+                                handler.postDelayed(this,200);
+                            }
+                        });
 
                     }
                 });
@@ -137,16 +180,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+
     }
 
     private void initData() {
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
+        handler=new Handler();
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
     }
 
     private void initView() {
         tv1=findViewById(R.id.tvAudio1);
         tv2=findViewById(R.id.tvAudio2);
         tv3=findViewById(R.id.tvAudio3);
+        seekBar=findViewById(R.id.pbProgressBar);
     }
 }
